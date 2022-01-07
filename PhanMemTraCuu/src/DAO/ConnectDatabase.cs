41df28f330d2c1f2d4dc24befb8bd592213c7f7b
Nguyễn Hữu An <nguyenhuuan2727@gmail.com>
2022-01-07 10:30:25 +0700

    first commit

---
+++ PhanMemTraCuu/src/DAO/ConnectDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace PhanMemTraCuu.src.DAO
+{
+    public class ConnectDatabase
+    {
+        public SqlConnection Connect()
+        {
+            return new SqlConnection(
+                @"Data Source=TOPXOAN\SQLEXPRESS;Initial Catalog=PhanMemTraCuuMonCTDL_GT1;User ID=sa;Password=0384974745"
+            );
+        }
+    }
+}
